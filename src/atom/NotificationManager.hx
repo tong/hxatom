5e2c18b23f544e61b87faa32acf4a7a50c1de075
tong <tong@disktree.net>
2015-06-05 13:13:27 +0200

    Fix

--- src/atom/NotificationManager.hx
+++ src/atom/NotificationManager.hx
@@ -4,11 +4,11 @@ extern class NotificationManager {
 
     function onDidAddNotification( callback : Notification->Void ) : Disposable;
 
-    function addSuccess( message : String, ?options : {detail:String} ) : Void;
-    function addInfo( message : String, ?options : {detail:String} ) : Void;
-    function addWarning( message : String, ?options : {detail:String} ) : Void;
-    function addError( message : String,?options : {detail:String} ) : Void;
-    function addFatalError( message : String, ?options : {detail:String} ) : Void;
+    function addSuccess( message : String, ?options : {?detail:String} ) : Void;
+    function addInfo( message : String, ?options : {?detail:String} ) : Void;
+    function addWarning( message : String, ?options : {?detail:String} ) : Void;
+    function addError( message : String,?options : {?detail:String} ) : Void;
+    function addFatalError( message : String, ?options : {?detail:String} ) : Void;
 
     function getNotifications() : Array<Notification>;
 
