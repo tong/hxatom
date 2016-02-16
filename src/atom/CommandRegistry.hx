3a68751ec9ae38bf0c17b3625412e379ccdc8d29
Tong <tong@disktree.net>
2016-02-16 18:07:13 +0100

    Fix

--- src/atom/CommandRegistry.hx
+++ src/atom/CommandRegistry.hx
@@ -1,9 +1,13 @@
 package atom;
 
+import js.html.Element;
+import js.html.Event;
+
 @:native("CommandRegistry")
 extern class CommandRegistry {
-    @:overload(function(target:String,commands:Dynamic):Disposable{})
-    function add( target : String, commandName : String, callback : Dynamic->Void ) : Disposable;
+    @:overload(function( target : String, commands : Dynamic ):Disposable{})
+    @:overload(function( target : Element, commandName : String, callback : Event->Void ):Disposable{})
+    function add( target : String, commandName : String, callback : Event->Void ) : Disposable;
     function findCommands( params : {target:js.html.Node} ) : Array<{name:String,displayName:String,jQuery:Dynamic}>;
     function dispatch( target : js.html.Node, commandName : String ) : Void;
 }
