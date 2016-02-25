2bab12eb3e71be8ffa6609fab51ead2f18c0d5ed
Tong <tong@disktree.net>
2016-02-25 04:57:43 +0100

    Add atom.Emitter

---
+++ src/atom/Emitter.hx
@@ -0,0 +1,12 @@
+package atom;
+
+@:jsRequire("atom","Emitter")
+extern class Emitter {
+    function new() : Void;
+    function clear() : Void;
+    function dispose() : Void;
+    function on( eventName : String, handler : Dynamic->Void, ?unshift : Bool ) : Disposable;
+    function preempt( eventName : String, handler : Void->Void ) : Disposable;
+    function off( eventName : String, handlerToRemove : Dynamic->Void ) : Void;
+    function emit( eventName : String, value : Dynamic ) : Void;
+}
