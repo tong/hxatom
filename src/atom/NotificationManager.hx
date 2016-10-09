829a401246633805d1652e86d9980b55fa2103de
tong <tong@disktree.net>
2016-10-09 15:11:31 +0200

    Fix

--- src/atom/NotificationManager.hx
+++ src/atom/NotificationManager.hx
@@ -1,15 +1,28 @@
 package atom;
 
-@:native("NotificationManager")
+typedef NotificationOptions = {
+    ?buttons : Array<{
+        ?className: String,
+        ?onDidClick: Void->Void,
+        ?text: String
+    }>,
+    ?description: String,
+    ?detail: String,
+    ?dismissable: Bool,
+    ?icon: String,
+    ?stack: String
+}
+
+@:jsRequire("atom","NotificationManager")
 extern class NotificationManager {
 
     function onDidAddNotification( callback : Notification->Void ) : Disposable;
 
-    function addSuccess( message : String, ?options : {?detail:String} ) : Void;
-    function addInfo( message : String, ?options : {?detail:String} ) : Void;
-    function addWarning( message : String, ?options : {?detail:String} ) : Void;
-    function addError( message : String,?options : {?detail:String} ) : Void;
-    function addFatalError( message : String, ?options : {?detail:String} ) : Void;
+    function addSuccess( message : String, ?options : NotificationOptions ) : Void;
+    function addInfo( message : String, ?options : NotificationOptions ) : Void;
+    function addWarning( message : String, ?options : NotificationOptions ) : Void;
+    function addError( message : String,?options : NotificationOptions ) : Void;
+    function addFatalError( message : String, ?options : NotificationOptions ) : Void;
 
     function getNotifications() : Array<Notification>;
 
